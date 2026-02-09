a21e43d2c5064621f3735dcbb8bef9a4330852cd
amitcoderhub <amitchauhan6599@gmail.com>
2026-02-09 22:50:55 +0530

    Django Form method

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import *
+
+# Create your views here.
+def contact(request):
+    ECFO = ContactForm()
+    d = {'ECFO': ECFO}
+    if request.method =='POST':
+        LFDO = ContactForm(request.POST)
+        if LFDO.is_valid():
+            return HttpResponse('Form is valid')
+        else:
+            return HttpResponse('Form is not valid')
+    return render(request, 'contact.html',d)
+        
